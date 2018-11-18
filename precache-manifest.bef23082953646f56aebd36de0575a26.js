bc4cc92f33abd2b4ea29a870c73ac09331503838
Lingyu Zhou <zhoulytwin@gmail.com>
2018-11-17 22:00:15 -0500

    Updates

---
+++ precache-manifest.bef23082953646f56aebd36de0575a26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/annotator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42b6d7aae30d4cdae690",
+    "url": "/annotator/static/js/runtime~main.42b6d7aa.js"
+  },
+  {
+    "revision": "fe471e262dccfaf7d10d",
+    "url": "/annotator/static/js/main.fe471e26.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/annotator/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "fe471e262dccfaf7d10d",
+    "url": "/annotator/static/css/main.4451d6b5.chunk.css"
+  },
+  {
+    "revision": "ca576a9ffb69c27ead771751d9735346",
+    "url": "/annotator/index.html"
+  }
+];
